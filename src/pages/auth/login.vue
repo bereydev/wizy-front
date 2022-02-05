cd6928c37f374f7e7fd9c2011f081cca2c68f631
Jonathan Bereyziat <jon.jbstar@gmail.com>
2022-02-05 16:50:09 +0100

    Vee-validate on login

--- src/pages/auth/login.vue
+++ src/pages/auth/login.vue
@@ -1,66 +1,36 @@
 <script setup lang="ts">
 // import type { Field } from "~/interface"
-import { useForm, useField } from 'vee-validate'
+import { Form, Field, ErrorMessage } from 'vee-validate'
 import * as yup from 'yup'
 import { useUserStore } from '~/stores/user'
 
 const userStore = useUserStore()
 const router = useRouter()
+const { login } = userStore
 
 // Define a validation schema
 const schema = yup.object({
-  username: yup.string().required().email(),
-  password: yup.string().required().min(8),
-});
-// Create a form context with the validation schema
-const { handleSubmit } = useForm({
-  validationSchema: schema,
+  username: yup.string().required("Champ obligatoir").email("Votre nom d'utilisateur doit être une addresse mail valide"),
+  password: yup.string().required("Champ obligatoir").min(8, "Votre mot de passe doit avoir au moins 8 charactères"),
 });
 
+async function onSubmit(values) {
 
-// No need to define rules for fields
-const { value: username, errorMessage: emailError } = useField('email');
-const { value: password, errorMessage: passwordError } = useField('password');
-
-const { login } = userStore
-
-function onSubmit() {
-  console.log("submitted")
+  try {
+    await login(values.username, values.password)
+    router.push("/")
+  } catch (error: any) {
+    if (error.response) {
+      console.table(error.response.data.detail)
+      if (error.response.status == 400) {
+        return
+      } else if (error.response.status == 422) {
+        return
+      }
+    }
+  }
 }
 
-// async function onSubmit() {
-
-//   try {
-//     await login(username.value, password.value)
-//     router.push("/")
-//   } catch (error: any) {
-//     if (error.response) {
-//       console.table(error.response.data.detail)
-//       if (error.response.status == 400) {
-//         return
-//       } else if (error.response.status == 422) {
-//         return
-//       }
-//     }
-//   }
-// }
-
-// Fields definition
-
-// const fieldUsername: Field = {
-//   value: username,
-//   label: "Adresse e-mail",
-//   name: "username",
-//   autocomplete: "username",
-// }
-
-// const fieldPassword: Field = {
-//   type: "password",
-//   value: password,
-//   name: "password",
-//   label: "Mot de passe",
-//   autocomplete: "current-password",
-// }
 
 </script>
 
@@ -71,13 +41,20 @@ function onSubmit() {
       class="font-semibold w-10/12 text-gray-600 text-sm"
     >Bon retour chez WizyTime le premier CRM français dédié aux coachs</p>
   </div>
-  <Form @submit.prevent="onSubmit" class="w-10/12 sm:w-8/12 md:w-6/12 lg:w-5/12 xl:w-4/12 mb-4">
-  <Field name="username" type="email" :rules="validateEmail" />
-      <ErrorMessage name="email" />
-    <input name="email" v-model="username" />
-    <span>{{ emailError }}</span>
-    <input name="password" v-model="password" type="password" />
-    <span>{{ passwordError }}</span>
+  <Form
+    @submit="onSubmit"
+    :validation-schema="schema"
+    class="w-10/12 sm:w-8/12 md:w-6/12 lg:w-5/12 xl:w-4/12 mb-4"
+  >
+    <div class="w-full mb-2">
+      <Field name="username" type="email" />
+      <ErrorMessage class="error" name="username" />
+    </div>
+    <div class="w-full mb-2">
+      <Field name="password" type="password" />
+      <ErrorMessage class="error" name="password" />
+    </div>
+
     <Button type="submit" class="w-full">Se connecter</Button>
   </Form>
   <div class="text-right w-10/12 sm:w-8/12 md:w-6/12 lg:w-5/12 xl:w-4/12 mb-4">
