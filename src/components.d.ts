930d7f307accc53c175521be168f3f1bb52b16de
Jonathan Bereyziat <jon.jbstar@gmail.com>
2022-02-17 10:04:00 +0100

    update deps

--- src/components.d.ts
+++ src/components.d.ts
@@ -4,36 +4,12 @@
 
 declare module 'vue' {
   export interface GlobalComponents {
-    AccountDropdown: typeof import('./components/AccountDropdown.vue')['default']
-    AvatarField: typeof import('./components/AvatarField.vue')['default']
-    Button: typeof import('./components/Button.vue')['default']
-    CalendarTable: typeof import('./components/calendar/CalendarTable.vue')['default']
-    CalendarView: typeof import('./components/calendar/CalendarView.vue')['default']
-    CalendarViewHeader: typeof import('./components/calendar/CalendarViewHeader.vue')['default']
-    ClientForm: typeof import('./components/forms/ClientForm.vue')['default']
-    ClientModal: typeof import('./components/ClientModal.vue')['default']
-    ClientRow: typeof import('./components/ClientRow.vue')['default']
-    EventCard: typeof import('./components/EventCard.vue')['default']
-    EventModal: typeof import('./components/EventModal.vue')['default']
-    FieldCustom: typeof import('./components/forms/FieldCustom.vue')['default']
-    FreeTimeline: typeof import('./components/FreeTimeline.vue')['default']
     'Iconoir:addUser': typeof import('~icons/iconoir/add-user')['default']
     'Iconoir:calendar': typeof import('~icons/iconoir/calendar')['default']
     'Iconoir:menu': typeof import('~icons/iconoir/menu')['default']
     IconoirArrowRight: typeof import('~icons/iconoir/arrow-right')['default']
     'IconParkOutline:close': typeof import('~icons/icon-park-outline/close')['default']
-    LoginForm: typeof import('./components/forms/LoginForm.vue')['default']
     'Logos:googleIcon': typeof import('~icons/logos/google-icon')['default']
-    Modal: typeof import('./components/Modal.vue')['default']
-    Navbar: typeof import('./components/Navbar.vue')['default']
-    QuickEventForm: typeof import('./components/forms/QuickEventForm.vue')['default']
-    RangeField: typeof import('./components/RangeField.vue')['default']
-    README: typeof import('./components/README.md')['default']
-    RegisterForm: typeof import('./components/forms/RegisterForm.vue')['default']
-    SelectField: typeof import('./components/forms/SelectField.vue')['default']
-    TextEditor: typeof import('./components/TextEditor.vue')['default']
-    ToolCard: typeof import('./components/ToolCard.vue')['default']
-    ToolForm: typeof import('./components/forms/ToolForm.vue')['default']
   }
 }
 
