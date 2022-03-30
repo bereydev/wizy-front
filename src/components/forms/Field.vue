3e058cde1c0183cac03fa30ea73c9a89ef76324c
Jonathan Bereyziat <jon.jbstar@gmail.com>
2022-03-30 23:30:42 +0200

    Custom field using vee-validate

--- src/components/forms/Field.vue
+++ src/components/forms/Field.vue
@@ -1,40 +1,33 @@
+<template>
+    <div>
+        <label :for="name">{{ label }}</label>
+        <input
+            class="py-2 px-2 mt-1 appearance-none border-2 block w-full border-gray-200 bg-white placeholder-gray-500 rounded focus:outline-none hover:border-indigo-400 hover:bg-indigo-100 focus:bg-white focus:border-indigo-700"
+            :id="name"
+            :name="name"
+            :type="type ?? 'text'"
+            :placeholder="placeholder ?? label"
+            :autocomplete="autocomplete"
+            v-model="value"
+        />
+        <span class="text-sm text-red-600" v-show="errorMessage">{{ errorMessage }}</span>
+    </div>
+</template>
+
+
 <script setup lang="ts">
+import { useField } from "vee-validate";
 
-import type { Field } from "~/interface"
-
-interface Props {
-    name: string
-    label: string
-    value: any
-    type?: string
-    placeholder?: string
-    id?: string
-    autocomplete?: string
-    error?: string
-}
-
-const props = withDefaults(defineProps<Props>(), {
-    type: "text",
+const props = defineProps({
+    name: String,
+    type : String,
+    label: String,
+    autocomplete : String,
+    placeholder : String,
 })
 
-const { value, error } = toRefs(props)
-let { name, type, label, placeholder, id } = props
+console.log(props.name)
 
-
-placeholder = placeholder == undefined ? label : placeholder
-id = id == undefined ? name : id
+const { value, errorMessage } = useField(props.name!);
 
 </script>
-
-<template>
-    <label :for="name" class="text-sm font-bold">{{ label }}</label>
-    <input
-        :id="id"
-        :type="type"
-        :placeholder="placeholder"
-        class="field"
-        :name="name"
-        v-model="value"
-    />
-    <span class="error">{{ error }}</span>
-</template>
